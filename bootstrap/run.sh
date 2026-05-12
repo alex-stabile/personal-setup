618a1d53e4a6c17ca2a23c302941ba79a21b9f19
alex-stabile <me@alexstabile.com>
2026-05-11 17:44:31 -0700

    cleanup

--- bootstrap/run.sh
+++ bootstrap/run.sh
@@ -5,12 +5,12 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # absolute path to personal-setup repo
 REPO_DIR="$(cd "$SCRIPT_DIR/.." && pwd)"
 
+echo "Running stow..."
 # stow zsh separately since it's ok if this fails
 if ! stow --dir "$REPO_DIR" --target "$HOME" zsh; then
   echo "stow zsh failed, continuing anyway"
 fi
-
 stow --dir "$REPO_DIR" --target "$HOME" tmux vim nvim
 
-echo "installing neovim plugins..."
+echo "Installing neovim plugins..."
 nvim --headless -u "$REPO_DIR/nvim/.config/nvim/lua/plugins.lua" +PlugInstall +qall
