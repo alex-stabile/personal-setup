4a8434951f36a90609b8a6aac6bc0f25c5b11849
alex-stabile <me@alexstabile.com>
2026-05-11 17:32:54 -0700

    extract plugins from init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,32 +1,4 @@
-local function install_plugins()
-  local Plug = vim.fn['plug#']
-  vim.call('plug#begin')
-  Plug('mason-org/mason.nvim')
-  Plug('neovim/nvim-lspconfig')
-  Plug('nvim-treesitter/nvim-treesitter', { ['branch'] = 'master', ['build'] = ':TSUpdate' })
-
-  Plug('github/copilot.vim')
-
-  -- use a release tag to download pre-built binaries.
-  Plug('saghen/blink.cmp', { ['tag'] = 'v1.*' })
-
-  Plug('stevearc/conform.nvim')
-  Plug('mfussenegger/nvim-lint')
-
-  Plug('junegunn/fzf')
-  Plug('junegunn/fzf.vim')
-
-  Plug('lewis6991/gitsigns.nvim')
-
-  Plug('tpope/vim-fugitive')
-  Plug('tpope/vim-rhubarb')
-
-  Plug('neanias/everforest-nvim', { ['branch'] = 'main' })
-  Plug('rebelot/kanagawa.nvim')
-  Plug('catppuccin/nvim', { ['as'] = 'catppuccin' })
-  vim.call('plug#end')
-end
-install_plugins()
+require('plugins')
 
 vim.o.termguicolors = true
 vim.o.background = 'dark'
